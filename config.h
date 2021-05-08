cc508d4a31c1479d24a8e34732c8aca88cb264f5
PedroSilva9 <ojosepedro@gmail.com>
2021-05-09 00:01:42 +0100

    added swallowing of emacs

--- config.h
+++ config.h
@@ -20,6 +20,9 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char emacsclient[] = "emacsclient";
+static const char emacsname[] = "emacs@";
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
